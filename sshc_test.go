cc3a1f190e96c134f8c94ab9a4661cf094022e8a
pyama <pyama@pepabo.com>
2021-05-20 11:44:36 +0900

    supported ProxyJump

--- sshc_test.go
+++ sshc_test.go
@@ -100,3 +100,49 @@ func TestGet(t *testing.T) {
 		t.Fatalf("want = %#v, got = %#v", want, got)
 	}
 }
+
+func TestConfig_parseProxyJump(t *testing.T) {
+	tests := []struct {
+		name    string
+		text    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "full config",
+			text:    "user@host:2002",
+			want:    "ssh -l %r -W %h:%p  user@host -p 2002",
+			wantErr: false,
+		},
+		{
+			name:    "not defined port",
+			text:    "user@host",
+			want:    "ssh -l %r -W %h:%p  user@host -p 22",
+			wantErr: false,
+		},
+		{
+			name:    "not defined user",
+			text:    "host:2222",
+			want:    "ssh -l %r -W %h:%p  host -p 2222",
+			wantErr: false,
+		},
+		{
+			name:    "wrong port format",
+			text:    "user@host:xxxxx",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := NewConfig("example.com")
+			got, err := c.parseProxyJump(tt.text)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Config.parseProxyJump() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Config.parseProxyJump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
